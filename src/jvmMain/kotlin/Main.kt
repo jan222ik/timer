5d505c929d9139d509fe12b149d6789c2e776bd1
Janik Mayr <32466915+jan222ik@users.noreply.github.com>
2023-03-21 21:14:30 +0100

    Window alignment with bottom left

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,13 +1,7 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,32 +10,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalWindowInfo
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import org.jetbrains.jewel.modifiers.background
-import org.jetbrains.jewel.themes.expui.desktop.window.LocalWindow
 import org.jetbrains.jewel.themes.expui.standalone.theme.DarkTheme
 
 @Composable
-@Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
+    Button(onClick = {
+        text = "Hello, Desktop!"
+    }) {
+        Text(text)
     }
 }
 
 fun main() = application {
     val theme = DarkTheme
-    val windowState = rememberWindowState()
+    val windowState = rememberWindowState(position = WindowPosition.Aligned(Alignment.BottomEnd))
     WindowsWindow(
         onCloseRequest = ::exitApplication,
         resizable = true,
